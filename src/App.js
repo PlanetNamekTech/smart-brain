27e9e2328786b83b8ecfcc520e8a80868df4f68f
PlanetNamekTech <sr23513@gmail.com>
2022-06-06 20:10:18 -0700

    replaced react script version and added image input

--- src/App.js
+++ src/App.js
@@ -1,112 +1,142 @@
 import './App.css';
+import { Component } from 'react';
 import Particles from "react-tsparticles";
+import Clarifai, { FACE_DETECT_MODEL } from 'clarifai';
 import { loadFull } from "tsparticles";
 import NavBar from './components/NavBar';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
-function App() {
-  const particlesInit = async (main) => {
-    console.log(main);
+const particlesInit = async (main) => {
+  // console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  // starting from v2 you can add only the features you need reducing the bundle size
+  await loadFull(main);
+};
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+const particlesLoaded = (container) => {
+  // console.log(container);
+};
 
-  const particlesOptions = {
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        resize: true,
+const particlesOptions = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
       },
-      modes: {
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
       },
     },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 2,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 100,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
     },
-    detectRetina: true,
-  }
-
-  return (
-
-    
-    
-    <div className="App">
-
-<Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={particlesOptions}
-    />
-      <NavBar />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      // outModes: {
+      //   default: "bounce",
+      // },
+      // Removing this property gets rid of the ludicrous speed problem
+      random: false,
+      speed: 2,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 100,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
 }
 
+const app = new Clarifai.App({
+  apiKey: 'ebe8566bf0554914961ce7b8f321b641'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(function(response) {
+      console.log(response);
+    },
+    function(err) {
+      console.log("Clarifai API not working",err)
+    })
+  }
+
+  render() {
+
+  return ( 
+      <div className="App">
+
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particlesOptions}
+      />
+        <NavBar />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
+}
 export default App;
